bd82393ab558ede2f0d1e1a78ae6588c3a20c3e0
bulat <boulatantonov@gmail.com>
2025-09-30 19:08:44 +0300

    migrate to 1.21.8

--- src/main/java/dev/tea/sugarpings/SugarPings.java
+++ src/main/java/dev/tea/sugarpings/SugarPings.java
@@ -1,33 +1,40 @@
 package dev.tea.sugarpings;
 
-import net.md_5.bungee.api.ChatColor;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Sound;
+import io.papermc.paper.event.player.AsyncChatEvent;
+import io.papermc.paper.util.Tick;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.TextComponent;
+import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.format.TextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import net.kyori.adventure.title.Title;
+import org.bukkit.*;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.Collection;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SugarPings extends JavaPlugin implements Listener {
     private static final Pattern HEX_PATTERN = Pattern.compile("&#[a-fA-F0-9]{6}");
-
+    private final TextReplacementConfig.Builder replacementConfigTemplateBuilder = TextReplacementConfig
+            .builder()
+            .match("%player%");
     private boolean needActionBar;
     private boolean needSound;
     private boolean needTitle;
     private boolean rangedChat;
-
-    private String actionBarTemplate;
+    private TextComponent actionBarTemplate;
     private Sound sound;
     private float soundVolume;
     private float soundPitch;
-    private String titleTemplate;
-    private String subtitleTemplate;
+    private TextComponent titleTemplate;
+    private TextComponent subtitleTemplate;
     private int titleFadeIn;
     private int titleStay;
     private int titleFadeOut;
@@ -44,7 +51,7 @@ public class SugarPings extends JavaPlugin implements Listener {
 
         actionBarTemplate = format(config.getString("ActionBar", ""));
         String soundId = config.getString("Sound.ID", "ENTITY_ARROW_HIT_PLAYER");
-        sound = Sound.valueOf(soundId);
+        sound = Registry.SOUNDS.getOrThrow(NamespacedKey.minecraft(soundId));
         soundVolume = (float) config.getDouble("Sound.Volume", 1.0);
         soundPitch = (float) config.getDouble("Sound.Pitch", 1.0);
         titleTemplate = format(config.getString("Title.Title", "&f"));
@@ -59,24 +66,24 @@ public class SugarPings extends JavaPlugin implements Listener {
         Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    private String format(String text) {
+    private @NotNull TextComponent format(String text) {
         Matcher matcher = HEX_PATTERN.matcher(text);
         while (matcher.find()) {
             String color = text.substring(matcher.start() + 1, matcher.end());
-            text = text.replace("&" + color, ChatColor.of(color) + "");
+            text = text.replace("&" + color, TextColor.fromHexString(color) + "");
             matcher = HEX_PATTERN.matcher(text);
         }
-        return ChatColor.translateAlternateColorCodes('&', text);
+        return LegacyComponentSerializer.legacy('&').deserialize(text);
     }
 
     @EventHandler
-    public void onChat(AsyncPlayerChatEvent event) {
-        String msgLower = event.getMessage().toLowerCase();
+    public void onChat(AsyncChatEvent event) {
+        String msgLower = ((TextComponent) event.message()).content().toLowerCase();
         Player sender = event.getPlayer();
         Location senderLoc = sender.getLocation();
         org.bukkit.World senderWorld = sender.getWorld();
 
-        Iterable<Player> candidates = rangedChat ? senderWorld.getPlayers() : Bukkit.getOnlinePlayers();
+        Collection<? extends Player> candidates = rangedChat ? senderWorld.getPlayers() : Bukkit.getOnlinePlayers();
 
         for (Player target : candidates) {
             if (rangedChat && target.getLocation().distanceSquared(senderLoc) >= rangeSquared) continue;
@@ -84,16 +91,33 @@ public class SugarPings extends JavaPlugin implements Listener {
             String targetLowerName = target.getName().toLowerCase();
             if (msgLower.contains(targetLowerName)) {
                 if (needActionBar) {
-                    String act = actionBarTemplate.replace("%player%", sender.getName());
+                    Component act = actionBarTemplate.replaceText(
+                            replacementConfigTemplateBuilder
+                                    .replacement(sender.getName())
+                                    .build()
+                    );
                     target.sendActionBar(act);
                 }
                 if (needSound) {
                     target.playSound(target.getLocation(), sound, soundVolume, soundPitch);
                 }
                 if (needTitle) {
-                    String title = titleTemplate.replace("%player%", sender.getName());
-                    String subtitle = subtitleTemplate.replace("%player%", sender.getName());
-                    target.sendTitle(title, subtitle, titleFadeIn, titleStay, titleFadeOut);
+                    Component title = titleTemplate.replaceText(
+                            replacementConfigTemplateBuilder
+                                    .replacement(sender.getName())
+                                    .build()
+                    );
+                    Component subtitle = subtitleTemplate.replaceText(
+                            replacementConfigTemplateBuilder
+                                    .replacement(sender.getName())
+                                    .build()
+                    );
+
+                    target.showTitle(Title.title(
+                            title,
+                            subtitle,
+                            Title.Times.times(Tick.of(titleFadeIn), Tick.of(titleStay), Tick.of(titleFadeOut))
+                    ));
                 }
             }
         }
